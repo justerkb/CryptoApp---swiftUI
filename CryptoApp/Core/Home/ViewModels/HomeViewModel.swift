a08c1601e759930bb8a42e5a2aa46d34183e9e5e
justerkb <erkebulan.nurbosyn@gmail.com>
2025-01-31 09:37:02 +0500

    added a logic of searchbar

--- CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
@@ -14,6 +14,13 @@ class HomeViewModel: ObservableObject {
     @Published var portfolioCoins: [CoinModel] = []
     @Published var searchedCoinText: String = ""
     
+    var searchedCoins: [CoinModel] {
+        return allCoins.filter { coin in
+            coin.name.lowercased().contains(searchedCoinText.lowercased()) ||
+            coin.symbol.lowercased().contains(searchedCoinText.lowercased())
+        }
+    }
+    
     private let dataService = CoinDataService()
     private var cancellables = Set<AnyCancellable>()
     
@@ -24,9 +31,26 @@ class HomeViewModel: ObservableObject {
     
     func addSubscribers() {
         
-        dataService.$allCoins
-            .sink { [weak self] coins in
-                self?.allCoins = coins
+//        dataService.$allCoins
+//            .sink { [weak self] coins in
+//                self?.allCoins = coins
+//            }
+//            .store(in: &cancellables)
+        
+        $searchedCoinText
+            .combineLatest(dataService.$allCoins)
+//            .debounce(for: 1, scheduler: DispatchQueue.main)
+            .map{ text, StartingCoins -> [CoinModel] in
+                if text.isEmpty { return StartingCoins }
+                
+                return StartingCoins.filter { coin in
+                    coin.name.lowercased().contains(text.lowercased()) ||
+                    coin.symbol.lowercased().contains(text.lowercased())
+                }
+
+            }
+            .sink { returnedCoins in
+                self.allCoins = returnedCoins
             }
             .store(in: &cancellables)
     }
